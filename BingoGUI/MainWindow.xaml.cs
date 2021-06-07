896c80595be32538d1effd1d9a4e93e8351802e2
MichaelVanHouHei <lai28hei@qq.com>
2021-06-08 03:32:25 +0800

    Adding files

---
+++ BingoGUI/MainWindow.xaml.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using BingoModel;
+
+namespace BingoGUI
+{
+    /// <summary>
+    /// MainWindow.xaml 的互動邏輯
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+
+        public ObservableCollection<BingoNumber> bingoNumbersList ;
+        public int min
+        {
+            get { return (int)GetValue(minProperty); }
+            set { SetValue(minProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for min.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty minProperty =
+            DependencyProperty.Register("min", typeof(int), typeof(MainWindow), new PropertyMetadata(1));
+
+
+
+        public int max
+        {
+            get { return (int)GetValue(maxProperty); }
+            set { SetValue(maxProperty, value); }
+        }
+
+        // Using a DependencyProperty as the backing store for max.  This enables animation, styling, binding, etc...
+        public static readonly DependencyProperty maxProperty =
+            DependencyProperty.Register("max", typeof(int), typeof(MainWindow), new PropertyMetadata(75));
+
+
+       
+         BingoGenerator generator;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            DataContext = this;
+            generator = new BingoGenerator(min, max);
+            bingoNumbersList = new ObservableCollection<BingoNumber>(generator.bingoNumbers.ToList());
+            panel.ItemsSource = bingoNumbersList;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            generator = new BingoGenerator(min, max);
+            bingoNumbersList = new ObservableCollection<BingoNumber>(generator.bingoNumbers.ToList());
+            panel.ItemsSource = bingoNumbersList;
+        }
+
+        private void GenClick(object sender, RoutedEventArgs e)
+        {
+            if (generator == null)
+            {
+                MessageBox.Show("請先按生成");
+                return;
+            }
+
+            if (generator.Generate())
+            {
+                bingoNumbersList = new ObservableCollection<BingoNumber>(generator.bingoNumbers.ToList());
+                panel.ItemsSource = bingoNumbersList;
+            }
+            else
+            {
+                MessageBox.Show("抽完");
+            }
+           
+        }
+    }
+}
---
+++ BingoModel/Generator.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BingoModel
+{
+    public class BingoNumber
+    {
+        public int value { get; set; }
+        public bool isSelected { get; set; }
+    }
+    public class BingoGenerator
+    {
+        private int min { get; set; }
+        private int max { get; set; }
+        public  List<BingoNumber> bingoNumbers = new List<BingoNumber>();
+
+        public BingoGenerator(int _min, int _max)
+        {
+           
+        }
+
+        private void init()
+        {
+           
+        }
+        
+        public bool Generate()
+        {
+            return true;
+        }
+        #region here is the answer , please do it your own first
+        //public BingoGenerator(int _min, int _max)
+        //{
+        //    min = _min;
+        //    max = _max;
+        //    init();
+        //}
+
+        //private void init()
+        //{
+        //    bingoNumbers = Enumerable.Range(min, max).Select(c => new BingoNumber() { value = c, isSelected = false })
+        //        .ToList();
+        //}
+        //Random rnd = new Random();
+        ////return true when there are number , false then there are no number
+        //public bool Generate()
+        //{
+        //    var result = bingoNumbers.Where(x => !x.isSelected).ToList();
+        //    if (result.Any())
+        //    {
+        //        var bingo = result[rnd.Next(result.Count)];
+        //        bingo.isSelected = true;
+        //        bingoNumbers[bingo.value - 1] = bingo;
+        //        return true;
+        //    }
+        //    return false;
+        //}
+
+        #endregion
+
+
+
+    }
+}
